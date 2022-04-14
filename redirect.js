39d5120d87b3abfc7663c6193fd6bb671065889d
Vince Rubinetti <vince.rubinetti@gmail.com>
2022-04-14 16:44:19 -0400

    add redirect code

---
+++ redirect.js
@@ -0,0 +1,10 @@
+// DO NOT EDIT OR FORMAT. GENERATED AUTOMATICALLY FROM PRIVATE REDIRECTS REPO.
+const list = "";
+
+const from = window.location.pathname.slice(1);
+const to = JSON.parse(list).find((item) => item?.from === from)?.to;
+
+if (from && to) {
+  console.log(`Redirecting from ${from} to ${to}`);
+  window.location.href = to;
+}
